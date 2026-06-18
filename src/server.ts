92f373b18a06eac0af62ec7e0dc6393fd9243aba
EbnulAhsan <ebnu1721@gmail.com>
2026-06-18 20:18:23 +0600

    fixed serverTS file

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,23 @@
 import app from './app';
-import config from './config';
 import pool, { createTables } from './db';
 import logger from './logger';
 
-const main = async () => {
+// DB connection run korbo (once)
+const initDB = async () => {
     try {
         await pool.query('SELECT NOW()');
         logger.info('Database connected successfully');
 
         await createTables();
-
         logger.info('Database tables are ready');
-
-
-        app.listen(config.port, () => {
-            logger.info(`DevPulse server is running on port ${config.port}`);
-        });
     } catch (error) {
         logger.error('Database connection failed');
         console.log(error);
     }
 };
 
-main();
+// run once
+initDB();
+
+// ❗❗ VERY IMPORTANT (Vercel needs this)
+export default app;
