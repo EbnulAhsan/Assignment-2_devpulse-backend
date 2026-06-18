e233684e4bfc59d93ec50f170eba3542039c3410
EbnulAhsan <ebnu1721@gmail.com>
2026-06-18 22:21:46 +0600

    fix db env issue2

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,28 @@
-// import app from './app';
-// import pool, { createTables } from './db';
-// import logger from './logger';
-
-// // DB connection run korbo (once)
-// const initDB = async () => {
-//     try {
-//         await pool.query('SELECT NOW()');
-//         logger.info('Database connected successfully');
-
-//         await createTables();
-//         logger.info('Database tables are ready');
-//     } catch (error) {
-//         logger.error('Database connection failed');
-//         console.log(error);
-//     }
-// };
-
-// // // run once
-// // initDB();
-
-// export default app;
-
-
-
 import app from './app';
+import pool, { createTables } from './db';
+import logger from './logger';
+
+// DB connection run korbo (once)
+const initDB = async () => {
+    try {
+        await pool.query('SELECT NOW()');
+        logger.info('Database connected successfully');
+
+        await createTables();
+        logger.info('Database tables are ready');
+    } catch (error) {
+        logger.error('Database connection failed');
+        console.log(error);
+    }
+};
+
+// // run once
+// initDB();
 
 export default app;
+
+
+
+// import app from './app';
+
+// export default app;
