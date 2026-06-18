5500f2a25b643babc07ec499ac44a6de835c14af
EbnulAhsan <ebnu1721@gmail.com>
2026-06-18 22:00:57 +0600

    clean reset backend

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,16 @@
-import app from "../src/app";
+import express from "express";
+import serverless from "serverless-http";
 
-export default (req: any, res: any) => {
-    return app(req, res);
-};
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("DevPulse API is running ✅");
+});
+
+app.get("/api/test", (req, res) => {
+    res.json({ message: "API working ✅" });
+});
+
+export default serverless(app);
